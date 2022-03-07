11dc643a607864a4a302466ec6c2fc29d756a0f9
Judekeyser <justin.dekeyser@gmail.com>
2022-03-07 15:51:06 +0100

    BUG-11 Better error messages

--- calang/tests/webengine/SystemFileContent.java
+++ calang/tests/webengine/SystemFileContent.java
@@ -2,6 +2,7 @@ package webengine;
 
 import webengine.src.FileContent;
 
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -9,6 +10,16 @@ import static java.util.Objects.requireNonNull;
 
 public interface SystemFileContent extends FileContent {
     default Path basePath() {
-        return Paths.get(requireNonNull(System.getenv("hcal-files")));
+        var hcalFiles = requireNonNull(System.getenv("hcal-files"),
+                "Unset environment variable hcal-files: please fill with an absolute path to the folder of hcal files"
+        );
+        try {
+            return Paths.get(hcalFiles);
+        } catch(InvalidPathException e) {
+            throw new IllegalArgumentException(
+                    "The computed path (from environment variable hcal-files=<%s>) cannot be converted to Path object".formatted(hcalFiles),
+                    e
+            );
+        }
     }
 }
